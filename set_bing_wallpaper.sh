9704890c1429761ac87ccf15733161328eb9d5a9
Andrii Semkiv <semkiv@gmail.com>
2015-09-23 16:36:49 +0300

    Update set_bing_wallpaper.sh

--- set_bing_wallpaper.sh
+++ set_bing_wallpaper.sh
@@ -17,7 +17,7 @@ idx="0"
 xmlURL="http://www.bing.com/HPImageArchive.aspx?format=xml&idx=$idx&n=1&mkt=$mkt"
 
 # $saveDir is used to set the location where Bing pics of the day are stored. $HOME holds the path of the current user's home directory
-saveDir=$HOME'/Pictures/Wallpapers/Bing/'
+saveDir="$HOME/MEGA/Pictures/Wallpapers/Bing/"
 
 # Set picture options
 # Valid options are: none,wallpaper,centered,scaled,stretched,zoom,spanned
@@ -30,24 +30,24 @@ picExt=".jpg"
 mkdir -p "$saveDir"
 
 # Download the highest resolution
-for picRes in _1920x1200 _1366x768 _1280x720 _1024x768; do
+for picRes in _1920x1080 _1920x1200 _1366x768 _1280x720 _1024x768; do
 
 	# Extract the relative URL of the Bing pic of the day from the XML data retrieved from xmlURL, form the fully qualified URL for the pic of the day, and store it in $picURL
-    	picURL="$bing$(echo "$(curl -s "$xmlURL")" | grep -oP "<urlBase>(.*)</urlBase>" | cut -d ">" -f 2 | cut -d "<" -f 1)$picRes$picExt"
+	picURL="$bing$(echo "$(curl -s "$xmlURL")" | grep -oP "<urlBase>(.*)</urlBase>" | cut -d ">" -f 2 | cut -d "<" -f 1)$picRes$picExt"
 
-    	# $picName contains the filename of the Bing pic of the day
-   	 	picName=$(basename "$picURL")
+	# $picName contains the filename of the Bing pic of the day
+	 	picName="$(basename "$picURL")"
 
-    	# Download the Bing pic of the day
-    	if curl -f -s -o $saveDir$picName $picURL; then
+	# Download the Bing pic of the day
+	if curl -f -s -o "$saveDir$picName" "$picURL"; then
 		# Set the GNOME3 wallpaper
-		PID="$(pgrep -n gnome-session)" DBUS_SESSION_BUS_ADDRESS="$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ | cut -d= -f2-)" DISPLAY=:0 dconf write "/org/gnome/desktop/background/picture-uri" "\"file://$saveDir$picName\""
+		PID="$(pgrep -n gnome-session)" DBUS_SESSION_BUS_ADDRESS="$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/"$PID"/environ|cut -d= -f2-)" DISPLAY=:0 dconf write "/org/gnome/desktop/background/picture-uri" "\"file://$saveDir$picName\""
 
 		# Set the GNOME 3 wallpaper picture options
-		PID="$(pgrep -n gnome-session)" DBUS_SESSION_BUS_ADDRESS="$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ | cut -d= -f2-)" DISPLAY=:0 dconf write "/org/gnome/desktop/background/picture-options" "\"$picOpts\""
-		
-		#Send the notification
-		PID="$(pgrep -n gnome-session)" DBUS_SESSION_BUS_ADDRESS="$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ | cut -d= -f2-)" DISPLAY=:0 notify-send "Bing wallpaper" "The Bing picture of the day \"$picName\" has been downloaded and set as your desktop wallpaper"
+		PID="$(pgrep -n gnome-session)" DBUS_SESSION_BUS_ADDRESS="$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/"$PID"/environ|cut -d= -f2-)" DISPLAY=:0 dconf write "/org/gnome/desktop/background/picture-options" "\"$picOpts\""
+
+		# Send the notification
+		PID="$(pgrep -n gnome-session)" DBUS_SESSION_BUS_ADDRESS="$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/"$PID"/environ|cut -d= -f2-)" DISPLAY=:0 notify-send "Bing wallpaper" "The Bing picture of the day \"$picName\" has been downloaded and set as your desktop wallpaper"
 
 		# Exit the script
 		exit 0
@@ -55,3 +55,4 @@ for picRes in _1920x1200 _1366x768 _1280x720 _1024x768; do
 done 
 
 exit 1
+
